9c1802309a5deaf7ff6f565cf9c4c97891c4f659
txdat <dattranx105@gmail.com>
2020-08-07 01:41:33 +0700

    [master] aaa

--- .zshrc
+++ .zshrc
@@ -107,3 +107,6 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+export PATH=$HOME/miniconda3/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/miniconda3/lib:$LD_LIBRARY_PATH
--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes
