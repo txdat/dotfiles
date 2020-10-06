1d8fb78069b6d778bfff9c97c170217137ca1a4d
txdat <dattranx105@gmail.com>
2020-10-07 00:14:19 +0700

    [master] update white_sur

--- white_sur.sh
+++ white_sur.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-mkdir .themes && cd .themes
+mkdir ~/.themes
+cd ~/.themes
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
 cd WhiteSur-gtk-theme && ./install.sh
-cd
-mkdir .icons && cd .icons
+mkdir ~/.icons
+cd ~/.icons
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme
 git clone https://github.com/vinceliuice/WhiteSur-cursors
 cd WhiteSur-icon-theme && ./install.sh
 cd ../WhiteSur-cursors && ./install.sh
-cd
+cd -
