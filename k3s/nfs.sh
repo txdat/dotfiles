08e6979af9f323373ddb0f18ca9764a8ed7697c7
txdat <dattranx105@gmail.com>
2023-01-14 23:16:23 +0700

    [master] minor update

--- k3s/nfs.sh
+++ k3s/nfs.sh
@@ -5,6 +5,7 @@ ip_addr="${2:-192.168.1.198}"
 sudo echo "${export_dir} ${ip_addr}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 sudo exportfs -arv
 
-cp nfs.yaml nfs1.yaml
-sed -i "s/{{export_dir}}/${export_dir}/g" nfs1.yaml
-sudo mv nfs1.yaml /var/lib/rancher/k3s/server/manifests
+cp nfs.yaml /tmp/nfs.yaml
+sed -i "s/{{export_dir}}/${export_dir}/g" /tmp/nfs.yaml
+sed -i "s/{{ip_addr}}/${ip_addr}/g" /tmp/nfs.yaml
+sudo mv /tmp/nfs.yaml /var/lib/rancher/k3s/server/manifests
