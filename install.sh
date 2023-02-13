db908b0ea7155ae1d407ffca6cbc442c7a979239
txdat <dattranx105@gmail.com>
2023-02-14 03:50:29 +0700

    [master] fix typo

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ Description=Update NVIDIA module in initcpio
 Depends=mkinitcpio
 When=PostTransaction
 NeedsTargets
-Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+Exec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
 EOL
 
 # for yoga14 -----------------------------------------------
