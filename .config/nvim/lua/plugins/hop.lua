d9d981dd206f239b44c2a8177e2d5163fb2c4278
txdat <dattranx105@gmail.com>
2023-02-24 18:37:54 +0700

    [master] nvim: update surround

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -4,5 +4,5 @@ local keymap = require("util").keymap
 
 keymap("n", "f", ":HopChar1CurrentLineAC<CR>")
 keymap("n", "F", ":HopChar1CurrentLineBC<CR>")
-keymap("n", "s", ":HopChar2AC<CR>")
-keymap("n", "S", ":HopChar2BC<CR>")
+keymap("n", "g", ":HopChar2AC<CR>")
+keymap("n", "G", ":HopChar2BC<CR>")
--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,1 +1,14 @@
-require("nvim-surround").setup({})
+require("nvim-surround").setup({
+    keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "sa",
+        normal_cur = "sca",
+        normal_line = "sA",
+        normal_cur_line = "scA",
+        visual = "sa",
+        visual_line = "sA",
+        delete = "sd",
+        change = "sc",
+    },
+})
