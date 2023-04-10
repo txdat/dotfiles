1fef1d3ae33a16eda395a70754700d3ec2bfc8c0
txdat <dattranx105@gmail.com>
2023-04-10 20:05:08 +0700

    [master] vim: update colorscheme

--- link_config.sh
+++ link_config.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 sudo ln -s ~/.dotfiles/.vimrc /root/.vimrc
+sudo ln -s ~/.dotfiles/.vim /root/.vim
 
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
