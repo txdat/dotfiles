89ac5c411142929f4d72bfa6cef4d30266c09328
txdat <dattranx105@gmail.com>
2023-05-10 13:55:04 +0700

    [master] nvim: update git

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -10,5 +10,8 @@ keymap("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
 keymap("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
 keymap("n", "<leader>hS", ":Gitsigns stage_buffer<CR>")
 keymap("n", "<leader>hR", ":Gitsigns reset_buffer<CR>")
+keymap("n", "<leader>hb", ":Gitsigns blame_line<CR>")
 keymap("n", "<leader>hi", ":Gitsigns preview_hunk_inline<CR>")
 keymap("n", "<leader>hd", ":Gitsigns diffthis<CR>")
+
+keymap({ "o", "x" }, "<leader>hx", ":Gitsigns select_hunk<CR>")
