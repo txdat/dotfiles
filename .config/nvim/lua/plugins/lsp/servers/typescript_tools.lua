fcba54c4dd7816918f34414ef7508ab060ae1479
txdat <dattranx105@gmail.com>
2023-06-23 11:34:16 +0700

    [master] nvim: minor update

--- .config/nvim/lua/plugins/lsp/servers/typescript_tools.lua
+++ .config/nvim/lua/plugins/lsp/servers/typescript_tools.lua
@@ -2,6 +2,6 @@ require("typescript-tools").setup({
     settings = {
         separate_diagnostic_server = true,
         publish_diagnostic_on = "insert_leave",
-        tsserver_plugins = { "typescript-styled-plugin" },
+        -- tsserver_plugins = { "typescript-styled-plugin" },
     },
 })
--- link_config.sh
+++ link_config.sh
@@ -6,6 +6,7 @@ sudo ln -s ~/.dotfiles/.vim /root/.vim
 ln -s ~/.dotfiles/.xprofile ~/.xprofile
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vimrc ~/.ideavimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.doom.d ~/.doom.d
