c95b27ec0484e372eb878e88bfe238ca3ef69152
txdat <dattranx105@gmail.com>
2023-06-27 23:16:11 +0700

    [master] nvim: update cmp

--- .config/nvim/lua/plugins/lsp/cmp.lua
+++ .config/nvim/lua/plugins/lsp/cmp.lua
@@ -129,13 +129,28 @@ cmp.setup {
     sorting = {
         priority_weight = 2,
         comparators = {
-            compare.offset,    -- we still want offset to be higher to order after 3rd letter
-            compare.score,     -- same as above
-            compare.sort_text, -- add higher precedence for sort_text, it must be above `kind`
-            compare.recently_used,
-            compare.kind,
-            compare.length,
-            compare.order,
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+
+            -- cmp-under
+            function(entry1, entry2)
+                local _, entry1_under = entry1.completion_item.label:find "^_+"
+                local _, entry2_under = entry2.completion_item.label:find "^_+"
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                    return false
+                elseif entry1_under < entry2_under then
+                    return true
+                end
+            end,
+
+            -- cmp.config.compare.kind,
+            -- cmp.config.compare.sort_text,
+            -- cmp.config.compare.length,
+            -- cmp.config.compare.order,
         },
     },
 }
