b8bb0be64a75cf786e6a4cc3b47f97208bf373fc
txdat <dattranx105@gmail.com>
2024-07-14 21:37:45 +0700

    update vim

--- .config/nvim/lua/plugins/colorscheme/moonfly.lua
+++ .config/nvim/lua/plugins/colorscheme/moonfly.lua
@@ -8,4 +8,8 @@ vim.g.moonflyUnderlineMatchParen = false
 vim.g.moonflyVirtualTextColor = true
 vim.g.moonflyWinSeparator = 0
 
+require("moonfly").custom_colors({
+    bg = "#000000",
+})
+
 vim.cmd("colorscheme moonfly")
