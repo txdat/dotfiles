4c543c8791433159100c18fb9d3cb642cb68866c
txdat <dattranx105@gmail.com>
2024-09-01 00:18:15 +0700

    update vim

--- .config/nvim/lua/plugins/lsp/servers_cfg.lua
+++ .config/nvim/lua/plugins/lsp/servers_cfg.lua
@@ -1,33 +1,33 @@
 local M = {}
 
-M.lua_ls = {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      runtime = {
-        version = "LuaJIT",
-        -- path = vim.split(package.path, ";"),
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME
-          -- "${3rd}/luv/library"
-          -- "${3rd}/busted/library",
-        }
-      },
-      completion = {
-        callSnippet = "Replace",
-      },
-      -- hint = { enable = true },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+-- M.lua_ls = {
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         globals = { "vim" },
+--       },
+--       runtime = {
+--         version = "LuaJIT",
+--         -- path = vim.split(package.path, ";"),
+--       },
+--       workspace = {
+--         checkThirdParty = false,
+--         library = {
+--           vim.env.VIMRUNTIME
+--           -- "${3rd}/luv/library"
+--           -- "${3rd}/busted/library",
+--         }
+--       },
+--       completion = {
+--         callSnippet = "Replace",
+--       },
+--       -- hint = { enable = true },
+--       telemetry = {
+--         enable = false,
+--       },
+--     },
+--   },
+-- }
 
 M.clangd = {
   cmd = {
@@ -48,54 +48,54 @@ M.clangd = {
   },
 }
 
-M.rust_analyzer = {
-  settings = {
-    ["rust-analyzer"] = {
-      imports = {
-        granularity = {
-          group = "module",
-        },
-        prefix = "self",
-      },
-      cargo = {
-        allFeatures = true,
-        loadOutDirsFromCheck = true,
-        runBuildScripts = true,
-      },
-      -- Add clippy lints for Rust.
-      checkOnSave = {
-        allFeatures = true,
-        command = "clippy",
-        extraArgs = { "--no-deps" },
-      },
-      procMacro = {
-        enable = true,
-        ignored = {
-          ["async-trait"] = { "async_trait" },
-          ["napi-derive"] = { "napi" },
-          ["async-recursion"] = { "async_recursion" },
-        },
-      },
-    },
-  },
-}
+-- M.rust_analyzer = {
+--   settings = {
+--     ["rust-analyzer"] = {
+--       imports = {
+--         granularity = {
+--           group = "module",
+--         },
+--         prefix = "self",
+--       },
+--       cargo = {
+--         allFeatures = true,
+--         loadOutDirsFromCheck = true,
+--         runBuildScripts = true,
+--       },
+--       -- Add clippy lints for Rust.
+--       checkOnSave = {
+--         allFeatures = true,
+--         command = "clippy",
+--         extraArgs = { "--no-deps" },
+--       },
+--       procMacro = {
+--         enable = true,
+--         ignored = {
+--           ["async-trait"] = { "async_trait" },
+--           ["napi-derive"] = { "napi" },
+--           ["async-recursion"] = { "async_recursion" },
+--         },
+--       },
+--     },
+--   },
+-- }
 
-M.gopls = {
-  init_options = {
-    usePlaceholders = true,
-    completeUnimported = true,
-  },
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      -- semanticTokens = true,
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
-}
+-- M.gopls = {
+--   init_options = {
+--     usePlaceholders = true,
+--     completeUnimported = true,
+--   },
+--   settings = {
+--     gopls = {
+--       analyses = {
+--         unusedparams = true,
+--       },
+--       -- semanticTokens = true,
+--       staticcheck = true,
+--       gofumpt = true,
+--     },
+--   },
+-- }
 
 M.pyright = {}
 
